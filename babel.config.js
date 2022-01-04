8966c3bac50d158fa53a79e9f7f6f9280159e57b
春洋 <1141079850@qq.com>
2022-01-04 15:34:48 +0800

    chore：调整配置文件

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,24 @@
+/* eslint-disable import/no-commonjs */
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
-const presets = [
-  [
-    'taro',
-    {
-      framework: 'react',
-      ts: true
-    },
-  ],
-];
-const plugins = [
-  [
-    'import',
-    {
-      libraryName: 'taro-hooks',
-      camel2DashComponentName: false
-    },
-    'taro-hooks',
-  ]
-];
-export {
-  presets,
-  plugins
-}
+module.exports = {
+    presets: [
+        [
+            "taro",
+            {
+                framework: "react",
+                ts: true
+            }
+        ]
+    ],
+    plugins: [
+        [
+            "import",
+            {
+                libraryName: "taro-hooks",
+                camel2DashComponentName: false
+            },
+            "taro-hooks"
+        ]
+    ]
+};
